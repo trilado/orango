8cb2847e24e69d18675c59f66d8251e281694366
Diego Oliveira <diegopso2@gmail.com>
2013-12-30 02:07:48 -0200

    alteracao de ckeditor para versao completa e adicao de tag span na lista de tags seguras.

--- app/wwwroot/js/ckeditor/config.js
+++ app/wwwroot/js/ckeditor/config.js
@@ -1,13 +1,12 @@
 /**
  * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For the complete reference:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
 	$.each(ckeditorConfig, function(k, o){
 		config[k] = o;
 	});
