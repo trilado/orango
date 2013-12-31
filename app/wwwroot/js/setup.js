873fbdb47a2174f9afc141016f00bee32a3deae1
Diego Oliveira <diegopso2@gmail.com>
2013-12-31 12:18:55 -0200

    correcao de detalhes no ckeditor.

--- app/wwwroot/js/setup.js
+++ app/wwwroot/js/setup.js
@@ -4,14 +4,14 @@ var ckeditorConfig = {
 		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline', '-', "Subscript", "Superscript"] },
 		{ name: 'lists', items : [ "NumberedList", "BulletedList", '-',"JustifyLeft","JustifyCenter", "JustifyRight", "JustifyBlock"] },
 		{ name: 'links', items : [ "Link", "Unlink"] },
-		{ name: 'typo', items : [ "Format", "Font", "FontSize", "TextColor"] },
+		{ name: 'typo', items : [ "Format", "FontSize", "TextColor"] },
 	],
 	toolbar_Page: [
 		{ name: 'historic', items : [ 'Undo','Redo'] },
 		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline', '-', "Subscript", "Superscript"] },
 		{ name: 'lists', items : [ "NumberedList", "BulletedList", '-',"JustifyLeft","JustifyCenter", "JustifyRight", "JustifyBlock"] },
 		{ name: 'links', items : [ "Link", "Unlink"] },
-		{ name: 'typo', items : [ "Format", "Font", "FontSize", "TextColor"] },
+		{ name: 'typo', items : [ "Format", "FontSize", "TextColor"] },
 	],
 	removeDialogTabs: 'image:advanced;link:advanced;link:target'
 };
